0d6ae0342e72b0ed9fb8405e59d624d844c10121
ASLawan <nawalnitsua@gmail.com>
2023-11-02 08:19:45 +0100

    Prevent code from execution upon import, make file pycodestyle compliant

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 import sys
-args = sys.argv
-num_args = len(args)
-if num_args == 1:
-    print("0 arguments.")
-elif num_args == 2:
-    print("{} argument:".format(num_args - 1))
-    print("{}: {}".format(1, args[1]))
-else:
-    print("{} arguments:".format(num_args - 1))
-    for i in range(1, num_args):
-        print("{}: {}".format(i, args[i]))
 
+
+def print_arguments():
+
+    args = sys.argv
+    num_args = len(args)
+    if num_args == 1:
+        print("0 arguments.")
+    elif num_args == 2:
+        print("{} argument:".format(num_args - 1))
+        print("{}: {}".format(1, args[1]))
+    else:
+        print("{} arguments:".format(num_args - 1))
+        for i in range(1, num_args):
+            print("{}: {}".format(i, args[i]))
+
+
+if __name__ == "__main__":
+    print_arguments()
