99a9f91b41377aba3730d7d1d8768c8e6b95dbaf
ASLawan <nawalnitsua@gmail.com>
2023-11-03 23:50:55 +0100

    Write a function that retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+
+    num = len(my_list)
+    if idx < 0:
+        return None
+    elif idx > num:
+        return None
+    else:
+        return my_list[idx]
+
+
+if __name__ == "__main__":
+    element_at(my_list, idx)
