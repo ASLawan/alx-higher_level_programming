9a381ccccbcf89637c454591b5bf7c4125fe470f
ASLawan <nawalnitsua@gmail.com>
2023-11-03 08:00:15 +0100

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 
 
 def main():
-    print("{}".format(len(sys.argv)))
+    
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
