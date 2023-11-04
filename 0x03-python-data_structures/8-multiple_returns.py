88f6573d6d7facdb7253971f88bc24c9e70f03ff
ASLawan <nawalnitsua@gmail.com>
2023-11-04 09:53:13 +0100

    Write a function that returns a tuple with the length and its first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    if sentence == "":
+        return None
+    else:
+        length = len(sentence)
+        letter = sentence[0]
+        return (length, letter)
+
+
+if __name__ == "__main__":
+    multiple_returns(sentence)
