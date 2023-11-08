2c83c84d61a2f084f9cb50755deac2bd341b40f5
ASLawan <nawalnitsua@gmail.com>
2023-11-08 17:21:20 +0100

    Make file pycodestyle compliant

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,7 +6,7 @@ def complex_delete(a_dictionary, value):
     for k, v in a_dictionary.items():
         if v == value:
             keys_to_delete.append(k)
-    
+
     for key in keys_to_delete:
         del a_dictionary[key]
 
