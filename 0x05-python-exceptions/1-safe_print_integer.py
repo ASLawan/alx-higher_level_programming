1ee4d87680795ee2e72022300e6e45bb3ec6487f
ASLawan <nawalnitsua@gmail.com>
2023-11-20 09:26:13 +0100

    Write a function that prints an integer withe the given format

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+
+    try:
+        print("{:d}".format(value),  end="\n")
+        return True
+    except (TypeError, ValueError):
+        return False
+
+
+if __name__ == "__main__":
+    safe_print_integer(value)
