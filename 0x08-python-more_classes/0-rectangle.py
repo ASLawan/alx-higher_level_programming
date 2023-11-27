69f47c9596e963937bd6f5c3c583ae16e8855fd3
ASLawan <nawalnitsua@gmail.com>
2023-11-27 15:27:19 +0100

    Add documentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+    Module with an empty class
+    class Rectangle
+
+"""
+
 
 class Rectangle:
-    """An empty class template for creating rectangle objects
+    """
+        An empty class template for creating rectangle objects
     """
     pass
