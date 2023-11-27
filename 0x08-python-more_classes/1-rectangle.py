7d043adbd0864c1542e68acf7491684f37a026de
ASLawan <nawalnitsua@gmail.com>
2023-11-27 10:03:53 +0100

    Write a class that defines a rectangle - instantiate width and height as private attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """Class Rectangle for creating rectangle objects
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        """Method to retrieve height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Method to set value of height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """ Method to retrieve width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Method to set value of width
+        """
+        if not isinstance(value, int):
+            raise TypeError("Width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
