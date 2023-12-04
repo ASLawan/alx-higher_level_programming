da28a97246bd79d981401a72a79d6769f09e5ddc
ASLawan <nawalnitsua@gmail.com>
2023-12-04 17:39:04 +0100

    Write a class Rectangle that inherits from BaseGeometry - compute area and print output as indicated

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    Module class BaseGeometry and class Rectangle
+    that inherits from Geometry
+
+"""
+
+
+class BaseGeometry:
+    """An empty class Base Geometry"""
+
+    def area(self):
+        """public method that computes area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates given value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from class
+        Geometry
+    """
+    def __init__(self, width, height):
+        """Method that initailiazes attributes"""
+        self.integer_validator("wdith", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Method that computes area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Method that prints formated output"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
