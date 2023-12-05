f39f47d83debc9446af3baba0e7f3c1f32561284
ASLawan <nawalnitsua@gmail.com>
2023-12-05 07:22:39 +0100

    Fix spelling of width

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         """Method that initailiazes attributes"""
-        self.integer_validator("wdith", width)
+        self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
