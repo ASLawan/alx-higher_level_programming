b379c0123af05afe32d63f65bbbee4f7ca920e89
ASLawan <nawalnitsua@gmail.com>
2023-12-05 16:58:24 +0100

    Write a class Student that defines student - retrieve dictionary representation as indicated

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    Module with class Student that defines
+    student
+
+"""
+
+
+class Student:
+    """class Student that defines student objects"""
+    def __init__(self, first_name, last_name, age):
+        """Method that initializes instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method that retrieves dictionary
+        representation of student instance
+        """
+        student = self.__dict__.copy()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return student
+
+            new_dict = {}
+
+            for i in range(len(attrs)):
+                for s_attr in student:
+                    if attrs[i] == s_attr:
+                        new_dict[s_attr] = student[s_attr]
+            return new_dict
+        return student
