80d538936fbf8fe6198f6c288862c8e3907d691a
ASLawan <nawalnitsua@gmail.com>
2023-12-05 07:45:34 +0100

    Fix output error - import module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python3
-"""
-    Module class BaseGeometry and class Rectangle
-    that inherits from Geometry
-
-"""
-
-
-class BaseGeometry:
-    """An empty class Base Geometry"""
-
-    def area(self):
-        """public method that computes area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates given value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
