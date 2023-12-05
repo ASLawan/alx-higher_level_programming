4abb50bb689ec8ad6e94e195818c490a0b5c53c3
ASLawan <nawalnitsua@gmail.com>
2023-12-05 12:35:00 +0100

    Write a function that reads a text file and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Module with function that opens reads
+    text file and prints it to stdout
+
+"""
+
+
+def read_file(filename=""):
+    """function that opens, reads and prints
+        text tile content
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        fcontent = f.read()
+        print(fcontent)
