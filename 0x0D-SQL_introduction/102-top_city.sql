80de8881d6123cc70e211e871076adf9faec441e
ASLawan <nawalnitsua@gmail.com>
2024-01-18 21:38:54 +0100

    Display top 3 cities temperatures for the months of July and August

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Display top 3 city temperatures
+-- script to display top 3 of cities temperatures  during July and August
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
