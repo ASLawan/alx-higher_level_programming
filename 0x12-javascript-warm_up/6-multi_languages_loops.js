7df317d5b1b0af146337aace3eef22efafc334e4
ASLawan <nawalnitsua@gmail.com>
2024-02-12 16:11:55 +0100

    Write a script that prints given sentences using a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loops.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = myArr.length;
+let i;
+
+for (i = 0; i < len; i++) {
+  console.log(myArr[i]);
+}
