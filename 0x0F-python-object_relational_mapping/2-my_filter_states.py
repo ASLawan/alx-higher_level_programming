88c147bfe140c12a687dccbd13456b206f729f58
ASLawan <nawalnitsua@gmail.com>
2024-02-16 17:06:42 +0100

    Write a script that takes an argument and display all values from database that match the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    Module that implements SQL select statement
+
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: < filename > < username > < password >\
+            < database > < state name searched >")
+        sys.exit(1)
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+        state_name = sys.argv[4]
+        port = 3306
+        db = MySQLdb.connect(
+                user=username,
+                passwd=password,
+                db=db_name,
+                port=port
+                )
+        cursor = db.cursor()
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC;"
+        cursor.execute(query, (state_name,))
+
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
