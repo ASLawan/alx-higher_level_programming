5edc2fcb08cbb395dd2d8831be8e7055cdcc3678
ASLawan <nawalnitsua@gmail.com>
2024-02-17 09:23:18 +0100

    Improve model_state file

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Module with class State that inherits from Base
+    declarative class
+
+"""
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        Class state that defines states
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete-orphan")
