1c1e67b210f3b12feed6abb04ee11257caad7411
ASLawan <nawalnitsua@gmail.com>
2024-02-18 13:56:26 +0100

    List all State objects and corresponding city objeects

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,7 +6,7 @@
 """
 import sys
 from sqlalchemy import create_engine
-from sqlachemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
@@ -20,15 +20,16 @@ if __name__ == "__main__":
         passwd = sys.argv[2]
         db = sys.argv[3]
 
-        eng = create_engine(f'mysql: //{user}: {passwd}@localhost: 3306/{db}')
+        eng = create_engine(f'mysql://{user}: {passwd}@localhost: 3306/{db}')
         Base.metadata.create_all(eng)
         Session = sessionmaker(bind=eng)
         session = Session()
 
-        states_cities = session.query(State).order_by(State.id, City.id).all()
+        states_cities = session.query(State).outerjoin(City)\
+            .order_by(State.id, City.id).all()
         for state in states_cities:
             print(f"{state.id}: {state.name}")
             for city in state.cities:
-                print(f"/t{city.id}: {city.name}")
+                print(f"\t{city.id}: {city.name}")
 
         session.close()
