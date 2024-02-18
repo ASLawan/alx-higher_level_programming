ef62aae4c437213e68c571030a819889dac8e9af
ASLawan <nawalnitsua@gmail.com>
2024-02-18 11:15:01 +0100

    Fix output errors

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,27 +11,18 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: < filename > < username >\
-                < password > < database >")
-        sys.exit(1)
-    else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        db_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-        engine = create_engine(f'mysql://{username}\
+    engine = create_engine(f'mysql://{username}\
                 :{password}@localhost:3306/{db_name}')
+    # Base.metadata.create_all(engine)
 
-        # Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        new_state = State(name='Louisiana')
-        session.add(new_state)
-        session.commit()
-
-        print(new_state.id)
-
-        session.close()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
