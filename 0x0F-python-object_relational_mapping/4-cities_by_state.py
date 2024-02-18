c0ceae3271c62a975d053a96476fbe1c628795c7
ASLawan <nawalnitsua@gmail.com>
2024-02-18 09:40:46 +0100

    Update: fix output and pycodestyle errors

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
                 port=3306
                 )
         cursor = db.cursor()
-        query = "SELECT cities.id, cities.name, state.name\
+        query = "SELECT cities.id, cities.name, states.name\
                 FROM cities\
                 JOIN states ON cities.state_id = states.id"
         cursor.execute(query)
