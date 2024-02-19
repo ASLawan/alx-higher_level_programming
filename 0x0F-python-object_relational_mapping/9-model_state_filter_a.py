ca92f86dd3918721fa1fea89e786b7818cff370c
ASLawan <nawalnitsua@gmail.com>
2024-02-19 11:18:32 +0100

    Update: use fomat method to print output

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
         username = sys.argv[1]
         password = sys.argv[2]
         db_name = sys.argv[3]
-        engine = create_engine(f'mysql://{username}\
-                :{password}@localhost:3306/{db_name}')
+        engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                               .format(username, password, db_name))
         # Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
@@ -28,6 +28,6 @@ if __name__ == "__main__":
             filter(State.name.like('%a%')).\
             order_by(State.id).all()
         for state in states:
-            print(f"{state.id}: {state.name}")
+            print("{}: {}".format(state.id, state.name))
 
         session.close()
