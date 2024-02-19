908cbe1fb52a420c97a22a4b31cf78ddf77bcbbe
ASLawan <nawalnitsua@gmail.com>
2024-02-19 11:11:27 +0100

    Update: use format method to print results

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,15 +18,15 @@ if __name__ == "__main__":
         username = sys.argv[1]
         password = sys.argv[2]
         db_name = sys.argv[3]
-        engine = create_engine(f'mysql+mysqldb://{username}\
-                :{password}@localhost:3306/{db_name}')
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(username, password, db_name))
         # Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
 
         state = session.query(State).order_by(State.id).first()
         if (state):
-            print(f"{state.id}: {state.name}")
+            print("{}: {}".format(state.id, state.name))
         else:
             print("Nothing")
         session.close()
