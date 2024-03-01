45872c4cf416f4b1d417114e4b900d5af322ddbc
ASLawan <nawalnitsua@gmail.com>
2024-03-01 17:29:00 +0100

    Write script to take url, sends a request to it, displays body of response - print error code if its greater than 400

---
+++ 0x11-python_network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Module to implement a script that displays error code
+    using requests package
+
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
