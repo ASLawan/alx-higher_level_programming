072c084d39f07d01de77bee1b3a12f328701fceb
ASLawan <nawalnitsua@gmail.com>
2024-03-01 22:32:51 +0100

    Correct urllib syntax

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode({'email': email})
+    data = parse.urlencode({'email': email})
     data = data.encode('ascii')
 
-    with urllib.request.urlopen(url, data) as response:
+    with request.urlopen(url, data) as response:
         body = response.read().decode('utf-8')
         print(body)
