18ffa4df54c6c4daab9358c993a43576499e7573
ASLawan <nawalnitsua@gmail.com>
2024-04-03 10:38:39 +0100

    Write a JS script that updates the text color of <header> element to red when user clicks given tag

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,9 @@
+$('#red_header').click(function () {
+  const header = $('header');
+
+  if (header.length > 0) {
+    header.css('color', '#FF0000');
+  } else {
+    console.error('Header element not found.');
+  }
+});
