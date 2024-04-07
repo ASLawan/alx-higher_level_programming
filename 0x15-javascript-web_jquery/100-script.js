d19d16018d8f161901098dce1767c836efb31808
ASLawan <nawalnitsua@gmail.com>
2024-04-07 07:38:20 +0100

    Write script to update header color to red - use querySelector

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  const headerElement = document.querySelector('header');
+
+  if (headerElement) {
+    headerElement.style.color = '#FF0000';
+  } else {
+    console.error('Header element not found');
+  }
+};
